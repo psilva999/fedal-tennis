e3c2103b4b18f67d74936cdf4314db14117cc2cd
PSILVA999 <p.silva0399@gmail.com>
2022-06-16 18:46:11 -0300

    Criei o switch shadow do dark mode

---
+++ src/Components/Events/darkAndWhiteMode.js
@@ -0,0 +1,3 @@
+export function darkAndWhiteMode() {
+  console.log("das")
+}
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 import Logo from '../Assets/svg/0-logo.svg'
 import Store from '../Assets/svg/1-buy.svg'
 
+import { darkAndWhiteMode } from './Events/darkAndWhiteMode.js'
+
 const Header = () => {
+
+
   return (
     <header>
 
@@ -16,6 +20,13 @@ const Header = () => {
           <li className='detail'>home</li>
           <li>contato</li>
           <li>news</li>
+
+          <button class="switch-container" 
+          onClick={ darkAndWhiteMode }>
+            <input type="checkbox" class="switch-shadow"/>
+            <label for="button"></label>
+          </button>
+
         </ul>
       </nav>
 
