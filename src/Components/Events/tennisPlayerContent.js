cb4bb8e5c138280f71db93d555f55daac5870354
PSILVA999 <p.silva0399@gmail.com>
2022-06-20 09:53:39 -0300

    Adicionei o efeito gradient quando clicar em um dos tênistas

---
+++ src/Components/Events/tennisPlayerContent.js
@@ -0,0 +1,11 @@
+export function federerContent() {
+  document.querySelector('.efeito-stories.federer').style.animation = 'show-loading .6s linear forwards'
+}
+
+export function gugaContent() {
+  document.querySelector('.efeito-stories.guga').style.animation = 'show-loading .6s linear forwards'
+}
+
+export function nadalContent() {
+  document.querySelector('.efeito-stories.nadal').style.animation = 'show-loading .6s linear forwards'
+}
--- src/Components/Tenistas.jsx
+++ src/Components/Tenistas.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
+import { federerContent, gugaContent, nadalContent } from './Events/tennisPlayerContent.js'
 
 import Federer from '../Assets/images/federer/0-federer.png'
 import Guga from '../Assets/images/guga/0-guga.png'
 import Nadal from '../Assets/images/nadal/0-nadal.png'
 
 const Tenistas = () => {
+
   return (
     <section className='tenistas'>
-      
-      <button>
-        <div className='efeito-stories'>
+
+      <button onClick={ federerContent }>
+        <div className='efeito-stories federer'>
           <img src={ Federer }/> </div>
 
         <span>Federer</span>
       </button>
 
-      <button>
-        <div className='efeito-stories'>
+      <button onClick={ gugaContent }>
+        <div className='efeito-stories guga'>
           <img src={ Guga }/> </div>
 
         <span>Guga</span>
       </button>
 
-      <button>
-        <div className='efeito-stories'>
+      <button onClick={ nadalContent }>
+        <div className='efeito-stories nadal'>
           <img src={ Nadal }/> </div>
 
         <span>Nadal</span>
